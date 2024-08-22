fa37c7678b5317f6fb07dbbca8f4a9224a8653e7
Yessirr300 <vvmm6825@gmail.com>
2024-08-23 00:19:38 +0200

    1

---
+++ src/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Shape[] shapes = new Shape[]{
+                new Circle(5),
+                new Triangle(3, 4),
+                new Square(2),
+                new Circle(7),
+                new Square(6)
+        };
+
+        double totalArea = calculateTotalArea(shapes);
+        System.out.println("Total area of all shapes: " + totalArea);
+    }
+
+    public static double calculateTotalArea(Shape[] shapes) {
+        double totalArea = 0;
+        for (Shape shape : shapes) {
+            totalArea += shape.getArea();
+        }
+        return totalArea;
+    }
+}
